448e61eacd0b24b97eee2dd163f16842eca75605
leverage2014 <myletter428@gmail.com>
2017-07-15 22:23:31 -0700

    add reducer

---
+++ test/reducer_spec.js
@@ -0,0 +1,59 @@
+import { Map, fromJS } from 'immutable';
+import { expect } from 'chai';
+
+import reducer from '../src/reducer';
+
+describe('reducer', () => {
+	it('handles SET_ENTRIES', () => {
+		const initialState = Map();
+		const action = {
+			type: 'SET_ENTRIES', 
+			entries: ['Trainspotting']
+		};
+		const nextState = reducer(initialState, action);
+
+		expect(nextState).to.equal(fromJS({
+			entries: ['Trainspotting']
+		}));
+	});
+
+	it('handles NEXT', () => {
+		const initialState = fromJS({
+			entries: ['Trainspotting', '28 days later']
+		});
+		const action = { type: 'NEXT' };
+		const nextState = reducer(initialState, action);
+
+		expect(nextState).to.equal(fromJS({
+			vote: {
+				pair: ['Trainspotting', '28 days later']
+			},
+			entries: []
+		}));
+	});
+
+	it('handles VOTE', () => {
+		const initialState = fromJS({
+			vote: {
+				pair: ['Trainspotting', '28 days later']
+			},
+			entries: []
+		});
+		const action = {
+			type: 'VOTE',
+			entry: 'Trainspotting'
+		};
+
+		const nextState = reducer(initialState, action);
+		expect(nextState).to.equal(fromJS({
+			vote: {
+				pair: ['Trainspotting', '28 days later'],
+				tally: {
+					Trainspotting: 1
+				}
+			},
+			entries: []
+		}));
+	});
+
+});
