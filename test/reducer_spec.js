7d9cbf2b74a4271bb3725c6b4c4de87152e6859f
leverage2014 <myletter428@gmail.com>
2017-07-15 23:05:52 -0700

    use reducer composition

--- test/reducer_spec.js
+++ test/reducer_spec.js
@@ -36,8 +36,7 @@ describe('reducer', () => {
 		const initialState = fromJS({
 			vote: {
 				pair: ['Trainspotting', '28 days later']
-			},
-			entries: []
+			}
 		});
 		const action = {
 			type: 'VOTE',
@@ -51,9 +50,36 @@ describe('reducer', () => {
 				tally: {
 					Trainspotting: 1
 				}
-			},
-			entries: []
+			}
 		}));
 	});
 
+	it('has an initial state', () => {
+		const action = {
+			type: 'SET_ENTRIES',
+			entries: ['Trainspotting']
+		};
+
+		const nextState = reducer(undefined, action);
+		expect(nextState).to.equal(fromJS({
+			entries: ['Trainspotting']
+		}));
+	});
+
+	it('can be used with reduce', () => {
+		const actions = [
+		    {type: 'SET_ENTRIES', entries: ['Trainspotting', '28 Days Later']},
+		    {type: 'NEXT'},
+		    {type: 'VOTE', entry: 'Trainspotting'},
+		    {type: 'VOTE', entry: '28 Days Later'},
+		    {type: 'VOTE', entry: 'Trainspotting'},
+		    {type: 'NEXT'}
+		];
+
+		const finalState = actions.reduce(reducer, Map());
+
+		expect(finalState).to.equal(fromJS({
+			winner: 'Trainspotting'
+		}));
+	});
 });
